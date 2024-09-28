2eab6c442765080cd37131c05cc179cd33ccf2d7
stackgen-automation <automation@stackgen.com>
2024-09-28 10:01:06 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,6 +1,24 @@
 
-output "aws_s3_sam_malviya_12_bucket_name" {
-  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_name
+output "aws_lambda_hello_kitty_funcky_sam_12_invoke_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_funcky_sam_12_function_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_funcky_sam_12_function_name" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
+  sensitive = false
+}
+
+
+output "aws_lambda_hello_kitty_funcky_sam_12_function_url" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_url
   sensitive = false
 }
 
@@ -19,6 +37,12 @@ output "aws_s3_sam_malviya_12_kms_arn" {
 
 output "aws_s3_sam_malviya_12_arn" {
   value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.arn
+  sensitive = false
+}
+
+
+output "aws_s3_sam_malviya_12_bucket_name" {
+  value = module.appcd_551e1d2b-88ec-5406-87cf-c51521bbe894.bucket_name
   sensitive = false
 }
 
@@ -43,29 +67,5 @@ output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_sam_12_arn" {
 
 output "aws_cloudwatch_log_group_aws_lambda_hello_kitty_function_sam_12_name" {
   value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_funcky_sam_12_function_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_funcky_sam_12_function_name" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_funcky_sam_12_function_url" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_url
-  sensitive = false
-}
-
-
-output "aws_lambda_hello_kitty_funcky_sam_12_invoke_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
   sensitive = false
 }
